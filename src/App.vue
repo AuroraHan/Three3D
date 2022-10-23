00ed6156787ff2480a35e2f71caa74c74af0e782
AuroraHan <1224195854@qq.com>
2022-10-23 18:20:46 +0800

    初始化three项目

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Test></Test>
+</template>
+
+<script setup lang="ts">
+import Test from "@/components/InitCom/Test.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/InitCom/Test.vue
@@ -0,0 +1,14 @@
+<!--  -->
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import ThreeJs from "./index";
+
+onMounted(() => {
+  new ThreeJs();
+});
+</script>
+<style scoped></style>
