cfe72d30873bdc976869586f313df1c94d14da77
AuroraHan <1224195854@qq.com>
2023-04-15 17:13:19 +0800

    three组成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <Test></Test>
+  <day01></day01>
 </template>
 
 <script setup lang="ts">
 import Test from "@/components/InitCom/Test.vue";
+import day01 from '@/components/0414/init.vue'
 </script>
 
 <style scoped></style>
