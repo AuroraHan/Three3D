87153a40a880842629642423476a064ff3660f18
guoxiaohan <guoxiaohan@pybctec.com>
2023-05-24 11:37:53 +0800

    添加格式化文档

--- src/hooks/useThree/core.ts
+++ src/hooks/useThree/core.ts
@@ -3,56 +3,59 @@ import { CSS2DRenderer } from "three/examples/jsm/renderers/CSS2DRenderer";
 import * as THREE from "three";
 
 class ThreeBase {
-  /**
-   * 初始化场景
-   * @returns scene
-   */
-  static initScene() {
-    const scene = new THREE.Scene();
-    return scene;
-  }
+	/**
+	 * 初始化场景
+	 * @returns scene
+	 */
+	static initScene() {
+		const scene = new THREE.Scene();
+		return scene;
+	}
 
-  /**
-   * 初始化相机
-   * @param element HTMLElement
-   * @returns camera
-   */
-  static initCamera(element: HTMLElement) {
-    const fov = 20;
-    const near = 0.1;
-    const far = 2000;
-    const aspect = element.offsetWidth / element.offsetHeight;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 0);
-    return camera;
-  }
+	/**
+	 * 初始化相机
+	 * @param element HTMLElement
+	 * @returns camera
+	 */
+	static initCamera(element: HTMLElement) {
+		const fov = 20;
+		const near = 0.1;
+		const far = 2000;
+		const aspect = element.offsetWidth / element.offsetHeight;
+		const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+		camera.position.set(0, 0, 0);
+		return camera;
+	}
 
-  static initCSSRender(element: HTMLElement) {
-    const CSSRenderer = new CSS2DRenderer();
-    CSSRenderer.setSize(element.offsetWidth, element.offsetHeight);
-    CSSRenderer.domElement.style.position = "absolute";
-    CSSRenderer.domElement.style.top = "0px";
-    element.appendChild(CSSRenderer.domElement);
-    return CSSRenderer;
-  }
+	static initCSSRender(element: HTMLElement) {
+		const CSSRenderer = new CSS2DRenderer();
+		CSSRenderer.setSize(element.offsetWidth, element.offsetHeight);
+		CSSRenderer.domElement.style.position = "absolute";
+		CSSRenderer.domElement.style.top = "0px";
+		element.appendChild(CSSRenderer.domElement);
+		return CSSRenderer;
+	}
 
-  static initRenderer(element: HTMLElement) {
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    // renderer.setClearColor('#000')
-    renderer.outputEncoding = THREE.sRGBEncoding;
-    renderer.shadowMap.enabled = true;
-    renderer.setSize(element.offsetWidth, element.offsetHeight);
-    renderer.localClippingEnabled = true;
-    element.appendChild(renderer.domElement);
-    return renderer;
-  }
+	static initRenderer(element: HTMLElement) {
+		const renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			alpha: true
+		});
+		// renderer.setClearColor('#000')
+		renderer.outputEncoding = THREE.sRGBEncoding;
+		renderer.shadowMap.enabled = true;
+		renderer.setSize(element.offsetWidth, element.offsetHeight);
+		renderer.localClippingEnabled = true;
+		element.appendChild(renderer.domElement);
+		return renderer;
+	}
 
-  static initControl(camera: THREE.Camera, domElement: HTMLElement) {
-    const control = new OrbitControls(camera, domElement);
-    control.target = new THREE.Vector3(0, 0, 0);
-    control.update();
-    return control;
-  }
+	static initControl(camera: THREE.Camera, domElement: HTMLElement) {
+		const control = new OrbitControls(camera, domElement);
+		control.target = new THREE.Vector3(0, 0, 0);
+		control.update();
+		return control;
+	}
 }
 
 export default ThreeBase;
--- src/hooks/useThree/index.ts
+++ src/hooks/useThree/index.ts
@@ -3,21 +3,21 @@ import { onMounted, shallowRef, ref } from "vue";
 import ThreeBase from "./core";
 
 function useThree() {
-  const container = ref<HTMLElement>();
-  const scene = shallowRef<THREE.Scene>();
-  const camera = shallowRef<THREE.Camera>();
+	const container = ref<HTMLElement>();
+	const scene = shallowRef<THREE.Scene>();
+	const camera = shallowRef<THREE.Camera>();
 
-  onMounted(() => {
-    const el = container.value as HTMLElement;
-    scene.value = ThreeBase.initScene();
-    camera.value = ThreeBase.initCamera(el);
-  });
+	onMounted(() => {
+		const el = container.value as HTMLElement;
+		scene.value = ThreeBase.initScene();
+		camera.value = ThreeBase.initCamera(el);
+	});
 
-  return {
-    container,
-    scene,
-    camera,
-  };
+	return {
+		container,
+		scene,
+		camera
+	};
 }
 
 export default useThree;
